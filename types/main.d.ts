0054f23dba32c5ded42d7dc0f6fbf7a3a2f73906
Paul Melnikow <github@paulmelnikow.com>
2019-07-18 16:50:49 -0400

    Add type declaration for forCases() (#56)

--- types/main.d.ts
+++ types/main.d.ts
@@ -2,8 +2,14 @@ export class TestCase {
   expect(returnValue: any): this
   describe(message: string): this
   should(message: string): this
-  assert(message: string, assertFunction: (actualReturnValue: any) => void): this
+  assert(
+    message: string,
+    assertFunction: (actualReturnValue: any) => void
+  ): this
 }
 
+export interface Given extends TestCase {}
+
 export function test(testFn: Function, definerFn: Function): void
-export function given(...args: any[]): TestCase
+export function given(...args: any[]): Given
+export function forCases(testCases: Given[]): TestCase
